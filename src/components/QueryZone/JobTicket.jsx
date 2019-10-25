cd4a355436410fe0d590fe4759370198046c90a8
Syhko <alexandregrauby@gmail.com>
2019-10-25 16:29:26 +0200

    add city name on ticket

--- src/components/QueryZone/JobTicket.jsx
+++ src/components/QueryZone/JobTicket.jsx
@@ -7,12 +7,24 @@ const JobTicket = ({
   companyName,
   jobSlug,
   companySlug,
-  handleClick
-}) => (
-  <div className="ticket" onClick={() => handleClick(jobSlug, companySlug)}>
-    <p className="ticket-job-text">{title}</p>
-    <p className="ticket-company-text">{companyName}</p>
-  </div>
-);
+  handleClick,
+  cities
+}) => {
+  function cityName() {
+    if (cities.length !== 0) {
+      return " - " + cities[0].name;
+    }
+    return "";
+  }
+  return (
+    <div className="ticket" onClick={() => handleClick(jobSlug, companySlug)}>
+      <p className="ticket-job-text">{title}</p>
+      <p className="ticket-company-text">
+        {companyName}
+        {cityName()}
+      </p>
+    </div>
+  );
+};
 
 export default JobTicket;
