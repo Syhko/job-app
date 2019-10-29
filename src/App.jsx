e191bc5596af04ffaa7db43811a9550436ccf128
Syhko <alexandregrauby@gmail.com>
2019-10-29 14:11:22 +0100

    new-route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./client";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AllJobs from "./routes/AllJobs/AllJobs";
+import JobPage from "./routes/JobPage/JobPage";
 import Home from "./routes/Home/Home";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/allJobs" component={AllJobs} />
+          <Route exact path="/job/:job_id" component={JobPage} />
         </Switch>
       </BrowserRouter>
     </>
